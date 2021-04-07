474fe5df6440f7143a77a81636686e11fa3f9b70
Erin <531590172@qq.com>
2021-04-07 18:56:46 +0800

    PR

--- Run.go
+++ Run.go
@@ -1,19 +1,25 @@
 package main
 
-import(
-    "fmt"
-    //"math/rand"
-    "math"
-    //"UserData"
-    //"os"
-    //"text/tabwriter"    
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"text/tabwriter"
 )
 
 func main() {
-    fmt.Println(Sqrt2(2), Sqrt2Other(2), math.Sqrt(2));
-    
-    var i int;
-    i = 10;
-    j:= 11;
-    fmt.Println("my go lang", math.Sqrt2, i, j);
+	fmt.Println("平方根: ", Sqrt2(2), Sqrt2Other(2), math.Sqrt(2))
+	SetAccountInformation("demo", "demo")
+	fmt.Println("全局变量: ", GetAccount(), GetPassWord())
+	fmt.Println(os.Getwd())
+	i := 10
+	j := 11
+	fmt.Println(rand.Int(), math.Sqrt2, i, j)
+
+	w := tabwriter.NewWriter(os.Stdout, 15, 4, 1, ' ', tabwriter.TabIndent)
+	fmt.Fprintln(w, "username\tfirstname\tlastname\t")
+	fmt.Fprintln(w, "sohlich\tRadomir\tSohlich\t")
+	fmt.Fprintln(w, "novak\tJohn\tSmith\t")
+	w.Flush()
 }
